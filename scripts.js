9283302bf47c921e291e8eb248fe8ca80c7a997d
Szubertos <1ta.szubert@gmail.com>
2026-03-28 12:33:24 +0100

    Fix overlapping ships.

--- scripts.js
+++ scripts.js
@@ -24,34 +24,54 @@ class Gameboard {
         this.hits = [];
         this.boardSize = boardSize;
         this.ships = [];
+        this.blockedTiles = [];
     }
 
     placeShip(length, start, direction) {
         let ship = new Ship(length);
         let coordinates = [];
+        //check if ship is longer than the board
         if (length>10) {
             throw new Error ('Ship reaches outside the board.');
         }
         //check if the ship fits
+        console.log(direction);
+        console.log(direction == 'horizontal');
         if (direction == 'horizontal') {
             if ((start[0] + length) > this.boardSize) {
                 throw new Error ('Ship reaches outside the board.');
             }
         } else {
+            console.log(direction);
             if ((start[1] + length) > this.boardSize) {
                 throw new Error ('Ship reaches outside the board.');
             }
         }
-
+        //place ship
         for (let i=0; i<length; i++) {
             if (direction == 'horizontal') {
-                let coodinate = [start[0]+i, start[1]];
-                coordinates[i] = coodinate;
+                let coordinate = [start[0]+i, start[1]];
+                coordinates[i] = coordinate;
+                for (let j=0; j<this.blockedTiles.length; j++) {
+                    if (this.blockedTiles[j][0] == coordinate[0] && this.blockedTiles[j][1] == coordinate[1]) {
+                        return "Ship overlaps another ship.";
+                    }
+                }
             } else {
-                let coodinate = [start[0], start[1]+i];
-                coordinates[i] = coodinate;
+                let coordinate = [start[0], start[1]+i];
+                coordinates[i] = coordinate;
+                for (let j=0; j<this.blockedTiles.length; j++) {
+                    if (this.blockedTiles[j][0] == coordinate[0] && this.blockedTiles[j][1] == coordinate[1]) {
+                        return "Ship overlaps another ship.";
+                    }
+                }
             }
         }
+        //add ship tiles to blocked tiles
+        for (let i=0; i<coordinates.length; i++) {
+            this.blockedTiles.push(coordinates[i]);
+        }
+        console.log(this.blockedTiles);
 
         let shipSaved = {Ship: ship, Coordinates: coordinates};
         this.ships.push(shipSaved);
@@ -65,9 +85,5 @@ class Gameboard {
     }
 
 }
-
-const three = function(){
-    return 3;
-};
 
 module.exports = Gameboard;
--- scripts.test.js
+++ scripts.test.js
@@ -28,5 +28,16 @@ test('return all ships', () => {
     GB2.placeShip(2, [3,3], 'horizontal');
     GB2.placeShip(3, [1,4], 'vertical');
     GB2.placeShip(2, [5,8], 'vertical');
-  expect(GB2.returnAllShips()).toEqual([{"Coordinates": [[0, 0], [1, 0]], "Ship": {"hits": 0, "length": 2, "sunk": false}}, {"Coordinates": [[3, 3], [4, 3]], "Ship": {"hits": 0, "length": 2, "sunk": false}}, {"Coordinates": [[1, 4], [1, 5], [1, 6]], "Ship": {"hits": 0, "length": 3, "sunk": false}}, {"Coordinates": [[5, 8], [5, 9]], "Ship": {"hits": 0, "length": 2, "sunk": false}}]);
+    expect(GB2.returnAllShips()).toEqual([{"Coordinates": [[0, 0], [1, 0]], "Ship": {"hits": 0, "length": 2, "sunk": false}}, {"Coordinates": [[3, 3], [4, 3]], "Ship": {"hits": 0, "length": 2, "sunk": false}}, {"Coordinates": [[1, 4], [1, 5], [1, 6]], "Ship": {"hits": 0, "length": 3, "sunk": false}}, {"Coordinates": [[5, 8], [5, 9]], "Ship": {"hits": 0, "length": 2, "sunk": false}}]);
+});
+
+test('return all ships except last two, check for ship intersection', () => {
+    let GB2 = new Gameboard(10);
+    GB2.placeShip(2, [0,8], 'horizontal');
+    GB2.placeShip(2, [3,3], 'horizontal');
+    GB2.placeShip(3, [2,6], 'vertical');
+    GB2.placeShip(2, [5,8], 'vertical');
+    GB2.placeShip(10, [0,9], 'horizontal');
+    GB2.placeShip(3, [3, 2], 'vertical');
+    expect(GB2.returnAllShips()).toEqual([{"Coordinates": [[0, 8], [1, 8]], "Ship": {"hits": 0, "length": 2, "sunk": false}}, {"Coordinates": [[3, 3], [4, 3]], "Ship": {"hits": 0, "length": 2, "sunk": false}}, {"Coordinates": [[2, 6], [2, 7], [2, 8]], "Ship": {"hits": 0, "length": 3, "sunk": false}}, {"Coordinates": [[5, 8], [5, 9]], "Ship": {"hits": 0, "length": 2, "sunk": false}}]);
 });
